b0802e0f3e74f81bc7055fd40f000eccf52dc363
manraog <you@example.com>
2020-11-04 05:39:43 +0000

    Nuevo código

--- main.py
+++ main.py
@@ -2,58 +2,67 @@ import argparse
 import random
 import time
 
-from flask import Flask, redirect, url_for
-from google.cloud import logging
+# [START trace_demo_imports]
+from flask import Flask
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.ext.stackdriver.trace_exporter import StackdriverExporter
+from opencensus.trace import execution_context
+from opencensus.trace.propagation import google_cloud_format
+from opencensus.trace.samplers import AlwaysOnSampler
+# [END trace_demo_imports]
+import requests
 
-# TRACE
-from opencensus.ext.stackdriver import trace_exporter as stackdriver_exporter
-import opencensus.trace.tracer
 
-def initialize_tracer(project_id):
-    exporter = stackdriver_exporter.StackdriverExporter(
-        project_id=project_id
-    )
-    tracer = opencensus.trace.tracer.Tracer(
-        exporter=exporter,
-        sampler=opencensus.trace.tracer.samplers.AlwaysOnSampler()
-    )
-
-    return tracer
-
-# APP
 app = Flask(__name__)
 
-
-@app.route('/', methods=['GET'])
-def root():
-    return redirect(url_for('index'))
+# [START trace_demo_middleware]
+propagator = google_cloud_format.GoogleCloudFormatPropagator()
 
 
-# [START trace_setup_python_quickstart]
-@app.route('/index.html', methods=['GET'])
-def index():
-    tracer = app.config['TRACER']
-    tracer.start_span(name='index')
-
-    # Add up to 1 sec delay, weighted toward zero
-    time.sleep(random.random() ** 2)
-    result = "Tracing requests"
-
-    tracer.end_span()
-    return result
-# [END trace_setup_python_quickstart]
+def createMiddleWare(exporter):
+    # Configure a flask middleware that listens for each request and applies automatic tracing.
+    # This needs to be set up before the application starts.
+    middleware = FlaskMiddleware(
+        app,
+        exporter=exporter,
+        propagator=propagator,
+        sampler=AlwaysOnSampler())
+    return middleware
+# [END trace_demo_middleware]
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter
+@app.route('/')
+def template_test():
+    # Sleep for a random time to imitate a random processing time
+    time.sleep(random.uniform(0, 0.5))
+    # Keyword that gets passed in will be concatenated to the final output string.
+    output_string = app.config['keyword']
+    # If there is no endpoint, return the output string.
+    url = app.config['endpoint']
+    if url == "":
+        return output_string, 200
+    # Endpoint is the next service to send string to.
+    data = {'body': output_string}
+    # [START trace_context_header]
+    trace_context_header = propagator.to_header(execution_context.get_opencensus_tracer().span_context)
+    response = requests.get(
+        url,
+        params=data,
+        headers={
+          'X-Cloud-Trace-Context' : trace_context_header}
     )
-    parser.add_argument(
-        '--project_id', help='Project ID you want to access.', required=True)
+    # [END trace_context_header]
+    return response.text + app.config['keyword']
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--keyword",  default="", help="name of the service.")
+    parser.add_argument("--endpoint", default="", help="endpoint to dispatch appended string, simply respond if not set")
     args = parser.parse_args()
-
-    tracer = initialize_tracer(args.project_id)
-    app.config['TRACER'] = tracer
-
-    app.run()
+    app.config['keyword'] = args.keyword
+    app.config['endpoint'] = args.endpoint
+    # [START trace_demo_create_exporter]
+    createMiddleWare(StackdriverExporter())
+    # [END trace_demo_create_exporter]
+    app.run(debug=True, host='0.0.0.0', port=8080)
--- startup.sh
+++ startup.sh
@@ -1,7 +1,19 @@
 #! /bin/bash
-apt update
+
+#Instalación de agentes de logging y monitoring
+curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh
+sudo bash add-monitoring-agent-repo.sh
+curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
+sudo bash add-logging-agent-repo.sh
+sudo apt-get update
+sudo apt-get install stackdriver-agent
+sudo apt-get install google-fluentd
+sudo apt-get install google-fluentd-catch-all-config-structured
+sudo service google-fluentd start
+
+#Instalación de aplicación
 apt -y install git python3 python3-pip
 git clone https://github.com/manraog/GCP-Compute-Operations-Demo.git
 cd GCP-Compute-Operations-Demo
-pip3 install requeriments.txt
+pip3 install -r requeriments.txt
 python3 main.py
